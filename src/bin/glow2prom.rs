2117e90abec0697527b836658200e25fa3276657
James Hannah <james.hannah@stfc.ac.uk>
2023-05-01 21:02:07 +0100

    much tidying

--- src/bin/glow2prom.rs
+++ src/bin/glow2prom.rs
@@ -1,5 +1,10 @@
 use core::panic;
-use std::{net::SocketAddr, sync::Mutex, time::Duration};
+use std::{
+    io::ErrorKind,
+    net::{SocketAddr, ToSocketAddrs},
+    sync::Mutex,
+    time::Duration,
+};
 
 use actix_web::{
     get,
@@ -12,9 +17,11 @@ use lazy_static::lazy_static;
 use prometheus::{IntGaugeVec, Opts, Registry, TextEncoder};
 use rumqttc::{AsyncClient, Event::Incoming, MqttOptions, Packet::Publish, QoS};
 use sd_notify::NotifyState;
-use tracing::info;
+use tracing::{info, debug};
 use tracing_actix_web::TracingLogger;
-use tracing_subscriber::{EnvFilter, prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{
+    prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt, EnvFilter,
+};
 
 lazy_static! {
     pub static ref REGISTRY: Registry =
@@ -30,27 +37,42 @@ lazy_static! {
     )
     .expect("energy metric can be created");
     pub static ref CUMULATIVE_ENERGY_DAY: IntGaugeVec = IntGaugeVec::new(
-        Opts::new("cumulative_energy_day_watt_hours", "Cumulative energy - day (Wh)"),
+        Opts::new(
+            "cumulative_energy_day_watt_hours",
+            "Cumulative energy - day (Wh)"
+        ),
         &["mpan"],
     )
     .expect("energy metric can be created");
     pub static ref CUMULATIVE_ENERGY_WEEK: IntGaugeVec = IntGaugeVec::new(
-        Opts::new("cumulative_energy_week_watt_hours", "Cumulative energy - week (Wh)"),
+        Opts::new(
+            "cumulative_energy_week_watt_hours",
+            "Cumulative energy - week (Wh)"
+        ),
         &["mpan"],
     )
     .expect("energy metric can be created");
     pub static ref CUMULATIVE_ENERGY_MONTH: IntGaugeVec = IntGaugeVec::new(
-        Opts::new("cumulative_energy_month_watt_hours", "Cumulative energy - month (Wh)"),
+        Opts::new(
+            "cumulative_energy_month_watt_hours",
+            "Cumulative energy - month (Wh)"
+        ),
         &["mpan"],
     )
     .expect("energy metric can be created");
     pub static ref IMPORT_UNIT_PRICE: IntGaugeVec = IntGaugeVec::new(
-        Opts::new("import_unit_price_centipence", "Cost per unit/kWh (centipence)"),
+        Opts::new(
+            "import_unit_price_centipence",
+            "Cost per unit/kWh (centipence)"
+        ),
         &["mpan", "supplier"],
     )
     .expect("power metric can be created");
     pub static ref IMPORT_PRICE_STANDING_CHARGE: IntGaugeVec = IntGaugeVec::new(
-        Opts::new("import_standing_charge_centipence", "Standing charge (centipence)"),
+        Opts::new(
+            "import_standing_charge_centipence",
+            "Standing charge (centipence)"
+        ),
         &["mpan", "supplier"],
     )
     .expect("power metric can be created");
@@ -129,8 +151,12 @@ async fn handle_publish(packet: Packet) -> Result<(), &'static str> {
 }
 
 #[tracing::instrument(skip_all)]
-async fn run_mqtt() {
-    let mut mqttoptions = MqttOptions::new("rumqtt-sync", "boris.xyzzy.uk", 1883);
+async fn run_mqtt(connect_to: SocketAddr) {
+    let mut mqttoptions = MqttOptions::new(
+        "rumqtt-sync",
+        connect_to.ip().to_string(),
+        connect_to.port(),
+    );
     mqttoptions.set_keep_alive(Duration::from_secs(5));
 
     let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
@@ -141,7 +167,10 @@ async fn run_mqtt() {
                 Incoming(Publish(evi)) => {
                     tokio::spawn(handle_publish(evi.try_into().unwrap()));
                 }
-                _ => continue,
+                other => {
+                    debug!("other event: {:?}",other);
+                    continue;
+                },
             },
             Err(_) => {
                 panic!("bad mqtt poll");
@@ -154,18 +183,17 @@ async fn run_mqtt() {
 async fn metrics(cache: web::Data<Mutex<String>>) -> HttpResponse {
     let data = cache.as_ref().lock().unwrap();
     HttpResponse::Ok()
-        .content_type("text/plain; version=0.0.4")
+        .content_type(prometheus::TEXT_FORMAT)
         .body(data.clone())
 }
 
-#[tracing::instrument(level="warn",skip_all)]
+#[tracing::instrument(level = "warn", skip_all)]
 async fn export_metrics(mc: Data<Mutex<String>>) {
     let encoder = TextEncoder::new();
     let r_metrics = REGISTRY.gather();
     let metout = encoder.encode_to_string(&r_metrics).unwrap();
     let mut d = mc.as_ref().lock().unwrap();
     *d = metout;
-
 }
 
 #[tracing::instrument(skip_all)]
@@ -182,26 +210,46 @@ async fn run_export_cacher(mc: Data<Mutex<String>>) {
 struct CmdArgs {
     #[arg(short, long, default_value = "[::1]:8080")]
     listen_addr: SocketAddr,
+    #[arg(short, long, default_value = "mqtt.example.com:1883")]
+    mqtt_host: String,
+}
+
+fn host_from_str(input: &str) -> Option<SocketAddr> {
+    input.to_socket_addrs().ok()?.next()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let t_filter = EnvFilter::try_from_default_env().or_else(|_|EnvFilter::try_new("info")).unwrap();
+    let t_filter = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
     let t_sub_fmt = tracing_subscriber::fmt::layer().with_target(false);
     let t_journald = tracing_journald::layer().ok();
-    tracing_subscriber::registry().with(t_filter).with(t_sub_fmt).with(t_journald).init();
+    tracing_subscriber::registry()
+        .with(t_filter)
+        .with(t_sub_fmt)
+        .with(t_journald)
+        .init();
 
     let args = CmdArgs::parse();
 
-    tokio::spawn(run_mqtt());
+    // let mqtt_host: SocketAddr = args.mqtt_host.to_socket_addrs().unwrap().next().unwrap();
+    let mqtt_host = host_from_str(&args.mqtt_host).ok_or(ErrorKind::InvalidData)?;
+
+    tokio::spawn(run_mqtt(mqtt_host));
     register_metrics().await;
 
     let d = Data::new(Mutex::new(String::from("# metrics will be available soon")));
     tokio::spawn(run_export_cacher(d.clone()));
     let _ = sd_notify::notify(true, &[NotifyState::Ready]);
-    HttpServer::new(move || App::new().app_data(Data::clone(&d)).wrap(TracingLogger::default()).service(metrics))
-        .bind(args.listen_addr)?
-        .run()
-        .await?;
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::clone(&d))
+            .wrap(TracingLogger::default())
+            .service(metrics)
+    })
+    .bind(args.listen_addr)?
+    .run()
+    .await?;
     todo!()
 }
